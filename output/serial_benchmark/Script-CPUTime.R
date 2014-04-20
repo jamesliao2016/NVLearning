caa0bc990a1ec27f9b0adc7f5e4fb33ed67a33d5
WANG Tong <tong.wang@nus.edu.sg>
2014-04-20 23:36:35 +0800

    [serial benchmark] update figures with new results from SC[2], SC[4], IC[2], and IC[4] models

--- output/serial_benchmark/Script-CPUTime.R
+++ output/serial_benchmark/Script-CPUTime.R
@@ -15,14 +15,14 @@ data_E <- read.table("NVLearning_Event.serial.txt", header=TRUE)[c("c", "alpha",
 data_Em <- read.table("NVLearning_Event_myopic.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_Em")]
 data_T <- read.table("NVLearning_Timing.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_T")]
 data_Tm <- read.table("NVLearning_Timing_myopic.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_Tm")]
-#data_B2 <- read.table("NVLearning_CheckpointB.M2.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_B2")]
+data_B2 <- read.table("NVLearning_CheckpointB.M2.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_B2")]
 data_B2m <- read.table("NVLearning_CheckpointB_myopic.M2.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_B2m")]
-#data_B4 <- read.table("NVLearning_CheckpointB.M4.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_B4")]
+data_B4 <- read.table("NVLearning_CheckpointB.M4.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_B4")]
 data_B4m <- read.table("NVLearning_CheckpointB_myopic.M4.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_B4m")]
-#data_A2 <- read.table("NVLearning_CheckpointA.M2.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_A2")]
+data_A2 <- read.table("NVLearning_CheckpointA.M2.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_A2")]
 data_A2m <- read.table("NVLearning_CheckpointA_myopic.M2.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_A2m")]
 #data_A4 <- read.table("NVLearning_CheckpointA.M4.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_A4")]
-#data_A4m <- read.table("NVLearning_CheckpointA_myopic.M4.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_A4m")]
+data_A4m <- read.table("NVLearning_CheckpointA_myopic.M4.serial.txt", header=TRUE)[c("c", "alpha", "beta", "CPUTime_A4m")]
 
 
 
@@ -32,15 +32,15 @@ data <- merge(data, data_E, by=c("c", "alpha", "beta"))
 data <- merge(data, data_Tm, by=c("c", "alpha", "beta"))
 data <- merge(data, data_T, by=c("c", "alpha", "beta"))
 data <- merge(data, data_B2m, by=c("c", "alpha", "beta"))
-#data <- merge(data, data_B2, by=c("c", "alpha", "beta"))
+data <- merge(data, data_B2, by=c("c", "alpha", "beta"))
 data <- merge(data, data_B4m, by=c("c", "alpha", "beta"))
-#data <- merge(data, data_B4, by=c("c", "alpha", "beta"))
+data <- merge(data, data_B4, by=c("c", "alpha", "beta"))
 data <- merge(data, data_A2m, by=c("c", "alpha", "beta"))
-#data <- merge(data, data_A2, by=c("c", "alpha", "beta"))
-#data <- merge(data, data_A4m, by=c("c", "alpha", "beta"))
+data <- merge(data, data_A2, by=c("c", "alpha", "beta"))
+data <- merge(data, data_A4m, by=c("c", "alpha", "beta"))
 #data <- merge(data, data_A4, by=c("c", "alpha", "beta")) 
 
-colnames(data) <- c("c", "alpha", "beta", "F", "Em", "E",  "Tm", "T", "SC[2]m", "SC[4]m", "IC[2]m")
+colnames(data) <- c("c", "alpha", "beta", "F", "Em", "E",  "Tm", "T", "SC[2]m", "SC[2]", "SC[4]m", "SC[4]", "IC[2]m", "IC[2]", "IC[4]m") #, "IC[4]")
 
 #reshape data
 require("reshape2")
@@ -50,13 +50,18 @@ data.long <- melt(data=data, id.vars=c("c","alpha", "beta"), variable.name="Mode
 pdf('Figure-CPUTime.pdf', width = 8, height = 8)
 
     boxplot(log10(CPUTime) ~ Model, data=data.long, xlab="Model", ylab="CPU Time (millisecond)", range=0,
-        at=c(1, 3.25,2.75, 5.25,4.75, 7.25, 9.25, 11.25), #show.names=c(1,1,0,1,0,1,0,1),
-        xaxt="n", yaxt="n", ylim=c(1,10),
-        boxlty=c(1, 2,1, 2,1, 2, 2, 2), boxfill=c("grey80", "grey98","grey80", "grey98","grey80", "grey98", "grey98", "grey98"), 
-        medlty=c(1, 2,1, 2,1, 2, 2, 2), medlwd=c(1, 1,1, 1,1, 1, 1, 1),#medcol=c("grey40", "grey40", "grey70","grey40", "grey70","grey40", "grey70","grey40", "grey70","grey40"), 
-        whisklty=c(1, 2,1, 2,1, 2, 2, 2), staplelty=c(1, 2,1, 2,1, 2, 2, 2) )
+        xaxt="n", yaxt="n", ylim=c(1.7, 10.3),
+        at=c(1, 3.25,2.75, 5.25,4.75, 7.25,6.75, 9.25,8.75, 11.25,10.75, 13.25), #,12.75),
+        boxlty=c(1, 2,1, 2,1, 2,1, 2,1, 2,1, 2), #,1) 
+        boxfill=c("grey80", "grey98","grey80", "grey98","grey80", "grey98","grey80", "grey98","grey80", "grey98","grey80", "grey98"), #,"grey80"), 
+        medlty=c(1, 2,1, 2,1, 2,1, 2,1, 2,1,  2), #,1), 
+        medlwd=c(1, 1,1, 1,1, 1,1, 1,1, 1,1, 1), #,1),
+        #medcol=c("grey40", "grey40", "grey70","grey40", "grey70","grey40", "grey70","grey40", "grey70","grey40"), 
+        whisklty=c(1, 2,1, 2,1, 2,1, 2,1, 2,1, 2), #,1), 
+        staplelty=c(1, 2,1, 2,1, 2,1, 2,1, 2,1, 2) #,1),
+    )
 
-    axis(side=1, at=c(1, 3, 5, 7, 9, 11), labels=c("F","E","T","SC[2]","SC[4]", "IC[2]"))
-    axis(side=2, at=seq(1, 10, 1), labels=parse(text=paste("10^", seq(1, 10, 1), sep="")), las=1)
+    axis(side=1, at=c(1, 3, 5, 7, 9, 11, 13), labels=c("F","E","T","SC[2]","SC[4]", "IC[2]", "IC[4]"))
+    axis(side=2, at=seq(2, 10, 1), labels=parse(text=paste("10^", seq(2, 10, 1), sep="")), las=1)
 
 dev.off()
